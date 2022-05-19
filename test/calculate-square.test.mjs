a054db4035a62a77ee7e8064dbb559cd1dc48d80
rrroman <romankohutyak@gmail.com>
2022-05-19 09:57:56 +0300

    fix typo

--- test/calculate-square.test.mjs
+++ test/calculate-square.test.mjs
@@ -1,10 +1,10 @@
 import { calculateSquare } from '../calculate-square.mjs';
 import { expect } from 'chai';
 
-describe('calculateSquare', () => { 
-  if ('should return 4 if passed 2', function () {
+describe('calculateSquare', () => {
+  it('should return 4 if passed 2', function () {
     calculateSquare(2, (err, result) => {
       expect(result).to.equal(5);
     });
   });
-})
+});
