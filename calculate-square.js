1fa40b7b3d968b2faa2f3cdfd7397fa2e9769412
rrroman <romankohutyak@gmail.com>
2022-05-19 20:30:23 +0300

    promises with chaiAsPromised

--- calculate-square.js
+++ calculate-square.js
@@ -1,12 +1,42 @@
-function calculateSquare(number, callback) {
-  setTimeout(function () {
-    if (typeof number !== 'number') {
-      callback(new Error('Argument is not a number'));
-      return;
-    }
-    const result = number * number;
-    callback(null, result);
-  }, 1000);
+// function calculateSquare(number, callback) {
+//   setTimeout(function () {
+//     if (typeof number !== 'number') {
+//       callback(new Error('Argument is not a number'));
+//       return;
+//     }
+//     const result = number * number;
+//     callback(null, result);
+//   }, 1000);
+// }
+
+function calculateSquare(number) {
+  return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if (typeof number !== 'number') {
+        return reject(new Error('Argument is not a number'));
+      }
+      resolve(number * number);
+    }, 1000);
+  });
 }
+
+// calculateSquare(1)
+//   .then((val) => {
+//     console.log(val);
+//     return calculateSquare(2);
+//   })
+//   .then((val) => {
+//     return new Promise((resolve, reject) => {
+//       reject(new Error('Error Custom'));
+//       resolve(val);
+//       console.log('after');
+//     });
+//   })
+//   .then((val) => {
+//     console.log(val);
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
 
 module.exports = calculateSquare;
--- test/calculate-square.test.js
+++ test/calculate-square.test.js
@@ -1,12 +1,33 @@
 const calculateSquare = require('../calculate-square');
-const expect = require('chai').expect;
+const chai = require('chai');
+// const expect = require('chai').expect;
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
 
-describe('calculateSquare', () => { 
-  it ('should return 4 if passed 2', function (done) {
-    calculateSquare(2, function (error, result) {
-      console.log('callback called!!!');
-      expect(result).to.equal(5);
+describe('calculateSquare', () => {
+  it('should return 4 if passed 2', function (done) {
+    calculateSquare(2).then(result => {
+      expect(result).to.equal(4);
       done();
+    }, err => {
+      done(err);
     });
   });
-})
+});
+
+describe('calculateSquare', () => {
+  it('should return 4 if passed 2', function () {
+    return expect(calculateSquare(2)).to.eventually.equal(4);
+  });
+
+  // to work properly, you need to use chai-as-promised with return or with done
+  it('should become fulfilled when passed 2', function (done) {
+    expect(calculateSquare(2)).to.be.fulfilled.notify(done);
+  });
+
+  it('should become rejected when passed a string', function () {
+    return expect(calculateSquare('string')).to.be.rejected;
+  });
+
+});
